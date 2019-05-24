2cdc647e26cca0880b489405d994733a25cb271b
renatomvp <renato@mvpdevup.com.br>
2019-05-24 16:07:17 -0300

    incluídos comentarios nos códigos dos forms Form2 e Form3

--- ReservaMysqlWinForms/Form2.cs
+++ ReservaMysqlWinForms/Form2.cs
@@ -14,18 +14,30 @@ namespace ReservaMysqlWinForms
 {
     public partial class Form2 : Form
     {
+        // objeto da classe ReservaTemporaria
+        // que vai receber os dados do objeto
+        // passado pelo Form1 como parametro
         ReservaTemporaria reservaTemporaria;
 
+        // Criado um objeto da classe Cliente
+        // Esse objeto será preenchido após incluir um novo cliente
+        // Esse objeto será adicionado ao Form3 como parametro do seu construtor
+        // o objeto vai com seus dados para o Form3
         Cliente cliente = new Cliente();
 
+        // Classe criada para acesso e persistencia de dados de cliente
         ClienteDados clienteDados = new ClienteDados();
 
+        // esse é o construtor do Form2 que tem um objeto da classe ReservaTemporaria como parametro
         public Form2(ReservaTemporaria reserva)
         {
             InitializeComponent();
 
+            // o objeto da classe ReservaTemporaria criado para este form
+            // está recebendo o objeto que foi passado como parametro pelo Form1
             reservaTemporaria = reserva;
-
+            // o TextBox txtTipoQuarto está recebendo o valor
+            // da propriedade TipoQuarto do objeto reservaTemporaria
             txtTipoQuarto.Text = reservaTemporaria.TipoQuarto;            
         }
 
@@ -43,6 +55,7 @@ namespace ReservaMysqlWinForms
         {
             clienteDados.Incluir(txtNomeCliente.Text, txtCpf.Text, txtTelefone.Text, txtEndereco.Text, txtEmail.Text);
 
+            // Preenchendo o objeto cliente com os dados digitados no Form2
             cliente.Id = 0;
             cliente.Nome = txtNomeCliente.Text;
             cliente.CPF = txtCpf.Text;
@@ -53,8 +66,10 @@ namespace ReservaMysqlWinForms
 
         private void btnOutroForm_Click(object sender, EventArgs e)
         {
+            // criada uma instancia do Form3
+            // e passado o objeto cliente como parametro do seu construtor
             Form3 TerceiroForm = new Form3(cliente);
-
+            // abrindo esta instancia do Form3
             TerceiroForm.Show();
         }
     }
--- ReservaMysqlWinForms/Form3.cs
+++ ReservaMysqlWinForms/Form3.cs
@@ -13,14 +13,21 @@ namespace ReservaMysqlWinForms
 {
     public partial class Form3 : Form
     {
+        // objeto da classe Cliente
+        // que vai receber os dados
+        // do objeto passado pelo Form2 como parametro
         Cliente clienteForm2;
 
+        // construtor do Form3 com um objeto da classe Cliente como parametro
         public Form3(Cliente cliente)
         {
             InitializeComponent();
 
+            // o objeto clienteForm2 recebe os valores
+            // do objeto cliente que for passado como parametro
+            // pelo Form2
             clienteForm2 = cliente;
-
+            // label lblCliente recebe o valor da proprieda Nome do objeto cliente
             lblCliente.Text = cliente.Nome;
         }
 
